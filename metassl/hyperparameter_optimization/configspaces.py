3f88ec94d96929e6122a06c16b1a4cb5ba3f7c89
diane-wagner <wagnerd@informatik.uni-freiburg.de>
2021-11-03 19:09:05 +0100

    create probability_augment configspaces

--- metassl/hyperparameter_optimization/configspaces.py
+++ metassl/hyperparameter_optimization/configspaces.py
@@ -2,21 +2,28 @@ import ConfigSpace as CS
 import ConfigSpace.hyperparameters as CSH
 
 
-def get_test_configspace():
+def get_imagenet_probability_augment_configspace():
     cs = CS.ConfigurationSpace()
-    val_metric = CSH.UniformIntegerHyperparameter(
-        "val_metric", lower=1, upper=30, log=False, default_value=15,
+    p_colorjitter = CSH.UniformFloatHyperparameter(
+        "p_colorjitter", lower=0, upper=1, log=False, default_value=0.8,
     )
-    cs.add_hyperparameters([val_metric])
+    p_grayscale = CSH.UniformFloatHyperparameter(
+        "p_grayscale", lower=0, upper=1, log=False, default_value=0.2,
+    )
+    p_gaussianblur = CSH.UniformFloatHyperparameter(
+        "p_gaussianblur", lower=0, upper=1, log=False, default_value=0.5,
+    )
+    cs.add_hyperparameters([p_colorjitter, p_grayscale, p_gaussianblur])
     return cs
 
 
-def get_data_augmentation_configspace():
-    # TODO: Think for an interesting configspace for optimizing data augmentations
+def get_cifar10_probability_augment_configspace():
     cs = CS.ConfigurationSpace()
-    num_ops = CSH.UniformIntegerHyperparameter(
-        "num_ops", lower=1, upper=16, log=False, default_value=5,
+    p_colorjitter = CSH.UniformFloatHyperparameter(
+        "p_colorjitter", lower=0, upper=1, log=False, default_value=0.8,
     )
-    cs.add_hyperparameters([num_ops])
-
+    p_grayscale = CSH.UniformFloatHyperparameter(
+        "p_grayscale", lower=0, upper=1, log=False, default_value=0.2,
+    )
+    cs.add_hyperparameters([p_colorjitter, p_grayscale])
     return cs
