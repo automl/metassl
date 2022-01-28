bca4d2a493fe8f294af18bacb770334609384b42
diane-wagner <wagnerd@informatik.uni-freiburg.de>
2022-01-28 16:49:08 +0100

    Sample fix validation set randomly

--- metassl/baselines/simsiam/get_sampler.py
+++ metassl/baselines/simsiam/get_sampler.py
@@ -1,8 +1,13 @@
 import numpy as np
+import torch
 from torch.utils.data import SubsetRandomSampler
 
 
 def get_train_valid_sampler(args, trainset):
+    # Use always the same validation set!
+    # generator = torch.Generator()
+    # generator.manual_seed(0)
+
     dataset_percentage_usage = 100  # TODO: Add flag for this
     num_train = int(len(trainset) / 100 * dataset_percentage_usage)
     indices = list(range(num_train))
@@ -11,13 +16,17 @@ def get_train_valid_sampler(args, trainset):
     if args.distributed:
         np.random.seed(args.seed)
         np.random.shuffle(indices)
+    else:
+        random_number_generator = np.random.default_rng(0)
+        random_number_generator.shuffle(indices)
 
     if np.isclose(args.valid_size, 0.0):
         train_idx, valid_idx = indices, indices
     else:
         train_idx, valid_idx = indices[split:], indices[:split]
 
-    valid_sampler = SubsetRandomSampler(valid_idx)
+    #  print(valid_idx[:10])
+    valid_sampler = SubsetRandomSampler(valid_idx)  # add generator?
 
     if args.distributed:
         # Not tested yet
